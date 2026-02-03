709122f3b25f419337fdc8b7a48cfb5fb14c7811
User <user@example.com>
2026-02-02 20:31:43 -0500

    Initial commit

---
+++ src/test/java/ca/humber/cpan228/online_learning_platform/OnlineLearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.humber.cpan228.online_learning_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
